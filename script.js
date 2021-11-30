12782f9191be9e06be257b04e555e1fa11eb15e1
Jcward31 <91399744+Jcward31@users.noreply.github.com>
2021-11-29 19:11:19 -0500

    Add files via upload
    "just adding to the html some"

---
+++ script.js
@@ -0,0 +1,19 @@
+//link color to current time
+
+moment().format('MMMM Do YYYY, h:mm:ss a'); // November 29th 2021, 6:16:30 pm
+moment().format('dddd');                    // Monday
+moment().format("MMM Do YY");               // Nov 29th 21
+moment().format('YYYY [escaped] YYYY');     // 2021 escaped 2021
+moment().format();                          // 2021-11-29T18:16:30-05:00
+
+
+
+//add event listener to time block adding text
+
+
+
+//after save button commits are made to local storage
+
+
+
+//saves persist through refreshing page 
