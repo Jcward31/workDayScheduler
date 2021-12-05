2fb24f53ddb6b899436ca64926dc6e5f18a74cdc
Jcward31 <91399744+Jcward31@users.noreply.github.com>
2021-12-05 18:57:27 -0500

    Add files via upload
    working on setting up an array also separating time blocks into  past, present, future

--- script.js
+++ script.js
@@ -1,14 +1,54 @@
+const moment = require("moment");
+
 var crntDay = document.getElementById("currentDay");
-var timeBlock = document.getElementById ("time-block");
+var timeBlock = document.getElementByClass ("time-block");
 var saveBtn = document.querySelectorAll("saveBtn");
 
 
+
+var Time9 = document.getElementById("9");
+var Time10 = document.getElementById("10");
+var Time11 = document.getElementById("11");
+var Time12 = document.getElementById("12");
+var Time1 = document.getElementById("1");
+var Time2 = document.getElementById("2");
+var Time3 = document.getElementById("3");
+var Time4 = document.getElementById("4");
+var Time5 = document.getElementById("5");
+
+
+var amPm = [Time9,Time10,Time11,Time12,Time1,Time2,Time3,Time4,Time5];
+
+
+
+//adding in date and time + linking color (event) to textarea
 var date = moment().format('MMMM Do YYYY, h:mm:ss a');
 crntDay.innerHTML=date;
 
-//adding in date and time + linking color (event) to textarea
 
+//setting up color change based on time
+var hour = moment().format('H');
 
+console.log(t);
+
+for (i = 0; i<9; i++) {
+
+var time = i + 9;
+var d =amPm[i];
+
+if (time < hour) {
+    timeBlock.classList.add("past");
+}
+
+if (time == hour) {
+    timeBlock.classList.add("present");
+}
+
+if (time > hour) {
+    timeBlock.classList.add("future");
+}
+
+}
 
 
 
@@ -20,4 +60,4 @@ crntDay.innerHTML=date;
 
 
 
-//saves persist through refreshing page 
+//load previous saves when paged loads
